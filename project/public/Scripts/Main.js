b920490f44b5f8fc5079ac3a057459d2bf5ba5cf
Amir-Sharafi-86 <Amirsharafi8899@gmail.com>
2025-02-04 12:44:43 +0330

    js navigation

--- project/public/Scripts/Main.js
+++ project/public/Scripts/Main.js
@@ -5,3 +5,16 @@ const MenuMobile = $.getElementById("Mobile-menu")
 toggleBtn.addEventListener("click" , function() {
     MenuMobile.classList.toggle("hidden")
 })
+let liItems = document.querySelectorAll(".li-items");
+
+liItems.forEach(function(e) {
+    e.addEventListener("click", function(es) {
+        es.preventDefault();
+        let liData = e.getAttribute("data-section");
+        let sectionTop = document.querySelector(`.${liData}`).offsetTop;
+        window.scrollTo ({
+            top : sectionTop - 113,
+            behavior  : "smooth"
+        })
+    });
+});
